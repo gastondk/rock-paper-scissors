ea11bae44a8ce93fad9ffe3e4f4660f31b9acbbb
go02-i <gastond.ortiz@gmail.com>
2022-07-27 18:51:57 -0300

    function playRound

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
 /// get random element from array
 
-let myArray = ['Rock', 'Paper', 'Scissors']
+let myArray = ['rock', 'paper', 'scissors']
 
 function getComputerChoice () {
     let randomItem =  myArray[Math.floor(Math.random()*myArray.length)];
     return randomItem;
 }
+
+
+let playerScore = 0
+let computerScore = 0
+
+
+
+/// function that plays a single round of Rock Paper Scissors.
+
+///let playerSelection = prompt('Rock, Paper or Scissors?')
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
+        let result1 = 'You win! Paper beats rock';
+        playerScore++
+        return result1;
+
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
+        let result2 = 'You win! Rock beats scissors';
+        playerScore++
+        return result2;
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
+        let result3 = 'You Win! Scissors beats paper';
+        playerScore++
+        return result3;
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
+        let result4 = 'You lose! Paper beats rock';
+        computerScore++
+        return result4;
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
+        let result5 = 'You lose! Rock beats scissors';
+        computerScore++
+        return result5;
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
+        let result6 = 'You lose! Scissors beats paper';
+        computerScore++
+        return result6;
+    }
+}
+
+
