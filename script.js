f7c261347685ed018c3d79c11aed5630e6382129
go02-i <gastond.ortiz@gmail.com>
2022-07-27 19:00:03 -0300

    Add tie cases in function playRound

--- script.js
+++ script.js
@@ -19,30 +19,36 @@ let computerScore = 0
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'rock') {
-        let result1 = 'You win! Paper beats rock';
+        let result1 = "You win! Paper beats rock";
         playerScore++
         return result1;
 
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'scissors') {
-        let result2 = 'You win! Rock beats scissors';
+        let result2 = "You win! Rock beats scissors";
         playerScore++
         return result2;
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'paper') {
-        let result3 = 'You Win! Scissors beats paper';
+        let result3 = "You Win! Scissors beats paper";
         playerScore++
         return result3;
     } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper') {
-        let result4 = 'You lose! Paper beats rock';
+        let result4 = "You lose! Paper beats rock";
         computerScore++
         return result4;
     } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock') {
-        let result5 = 'You lose! Rock beats scissors';
+        let result5 = "You lose! Rock beats scissors";
         computerScore++
         return result5;
     } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors') {
-        let result6 = 'You lose! Scissors beats paper';
+        let result6 = "You lose! Scissors beats paper";
         computerScore++
         return result6;
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection === 'paper') {
+        return "It's a tie!"
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'rock') {
+        return "It's a tie!"
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'scissors') {
+        return "It's a tie!"
     }
 }
 
